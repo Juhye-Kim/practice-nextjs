475eaf2f59eeb77cecfd19da5aa5f2ab3aedbbaa
Juhye-Kim <kjh960215@gmail.com>
2022-05-14 15:03:17 +0900

    정적 파일 추가

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,21 @@
-import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const App = () => {
+    const [name, setName] = useState("");
+    const router = useRouter();
+
     return (
         <div>
-            <h2>Link to 'potato' page</h2>
-            <Link href="/vegetable/[name]" as="/vegetable/potato">
-                <a>Move to ''</a>
-            </Link>
+            <img src="/icon.png" />
+            <button type="button" onClick={() => router.push("/tomato")}>
+                tomato로 이동하기!
+            </button>
+            <p>이름</p>
+            <input value={name} onChange={(e) => setName(e.target.value)} style={{marginRight: "12px"}} />
+            <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
+                {name}으로 이동하기!
+            </button>
         </div>
     )
 }
