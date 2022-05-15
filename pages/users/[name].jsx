9ddeb3cb570a28eac25ac4ac62df275f9378c6d2
Juhye-Kim <kjh960215@gmail.com>
2022-05-16 00:05:10 +0900

    ch2: Repository list

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -26,6 +26,38 @@ const style = css`
         background-color: rgba(27, 31, 35, 0.08);
         border-radius: 20px;
     }
+    .repository-wrapper {
+        width: 100%;
+        border-bottom: 1px solid #e1e4e8;
+        padding: 24px 0;
+      }
+    .repository-description {
+        padding: 12px 0;
+    }
+    a {
+        text-decoration: none;
+    }
+    .repository-name {
+        margin: 0;
+        color: #0366d6;
+        font-size: 20px;
+        display: inline-block;
+        cursor: pointer;
+    }
+    .repository-name:hover {
+        text-decoration: underline;
+    }
+    .repository-description {
+        margin: 0;
+        font-size: 14px;
+    }
+    .repository-language {
+        margin: 0;
+        font-size: 14px;
+    }
+    .repository-updated-at {
+        margin-left: 20px;
+    }
 `
 
 const name = ({user, repos}) => {
@@ -41,6 +73,22 @@ const name = ({user, repos}) => {
                     Repsitories
                     <span className="repos-count">{user.public_repos}</span>
                 </div>
+                {repos.map((repo) => (
+                    <div key={repo.id} className="repository-wrapper">
+                        <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={`https://github.com/${user.login}/${repo.name}`}
+                        >
+                            <h2 className="repository-name">{repo.name}</h2>    
+                        </a>
+                        <p className="repository-description">{repo.description}</p>
+                        <p className="repository-language">
+                        {repo.language}
+                        <span className="repository-updated-at"></span>
+                        </p>
+                    </div>
+                ))}
             </div>
             <style jsx>{style}</style>
         </div>
