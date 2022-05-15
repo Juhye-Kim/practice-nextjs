e0afb03bf8632514696ecec698f1af73cf1cc5c8
Juhye-Kim <kjh960215@gmail.com>
2022-05-16 00:01:37 +0900

    ch2: Repository header

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,15 +1,49 @@
 import fetch from "isomorphic-unfetch";
 import Profile from "../../components/Profile";
+import css from "styled-jsx/css";
 
-const name = ({user}) => {
+const style = css`
+    .repos-wrapper {
+        width: 100%;
+        height: 100vh;
+        overflow: scroll;
+        padding: 0px 16px;
+    }
+    .repos-header {
+        padding: 16px 0;
+        font-size: 14px;
+        font-weight: 600;
+        border-bottom: 1px solid #e1e4e8;
+    }
+    .repos-count {
+        display: inline-block;
+        padding: 2px 5px;
+        margin-left: 6px;
+        font-size: 12px;
+        font-weight: 600;
+        line-height: 1;
+        color: #586069;
+        background-color: rgba(27, 31, 35, 0.08);
+        border-radius: 20px;
+    }
+`
+
+const name = ({user, repos}) => {
     if (!user) {
         return null;
     }
 
     return (
-        <>
+        <div className="user-contents-wrapper">
             <Profile user={user} />
-        </>
+            <div className="repos-wrapper">
+                <div className="repos-header">
+                    Repsitories
+                    <span className="repos-count">{user.public_repos}</span>
+                </div>
+            </div>
+            <style jsx>{style}</style>
+        </div>
     )
 }
 
@@ -17,15 +51,23 @@ export const getServerSideProps = async ({query}) => {
     const {name} = query;
 
     try {
-        const res = await fetch(`https://api.github.com/users/${name}`);
+        let user;
+        let repos;
 
-        if (res.status === 200) {
-            const user = await res.json();
+        const userRes = await fetch(`https://api.github.com/users/${name}`);
 
-            return {props: {user}};
+        if (userRes.status === 200) {
+            user = await userRes.json();
         }
-        return {props: {}};
 
+        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?sort=updated&page=1&per_page=10`);
+
+        if (repoRes.status === 200) {
+            repos = await repoRes.json();
+        }
+
+        console.log(repos);
+        return {props: {user, repos}};
     } catch (e) {
         console.log(e);
         return {props: {}};
