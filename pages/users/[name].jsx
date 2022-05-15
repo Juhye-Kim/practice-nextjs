a5fc789627bec5ff9cf1051c4c30ccb1a6f41b55
Juhye-Kim <kjh960215@gmail.com>
2022-05-16 00:45:25 +0900

    ch2: Error page file

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -11,40 +11,45 @@ const style = css`
 `;
 
 const name = ({ user, repos }) => {
-    return (
-      <div className="user-contents-wrapper">
-        <Profile user={user} />
-        <Repositories user={user} repos={repos} />
-        <style jsx>{style}</style>
-      </div>
-    );
-  };
+  return (
+    <div className="user-contents-wrapper">
+      <Profile user={user} />
+      <Repositories user={user} repos={repos} />
+      <style jsx>{style}</style>
+    </div>
+  );
+};
 
-export const getServerSideProps = async ({query}) => {
-    const {name, page} = query;
+export const getServerSideProps = async ({ query }) => {
+  const { name, page = "1" } = query;
+  try {
+    let user;
+    let repos;
 
-    try {
-        let user;
-        let repos;
-
-        const userRes = await fetch(`https://api.github.com/users/${name}`);
-
-        if (userRes.status === 200) {
-            user = await userRes.json();
-        }
-
-        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`);
-
-        if (repoRes.status === 200) {
-            repos = await repoRes.json();
-        }
-
-        console.log(repos);
-        return {props: {user, repos}};
-    } catch (e) {
-        console.log(e);
-        return {props: {}};
+    const userRes = await fetch(`https://api.github.com/users/${name}`);
+    if (userRes.status === 200) {
+      user = await userRes.json();
+    } else {
+      throw Error(userRes.statusText);
     }
-}
+    const repoRes = await fetch(
+      `https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`,
+      {
+        headers: {
+          Authorization: "token 714e48da28db7533dd9dafe6f4f040b142d30560",
+        },
+      }
+    );
+    if (repoRes.status === 200) {
+      repos = await repoRes.json();
+    } else {
+      throw Error(userRes.statusText);
+    }
+    return { props: { user, repos } };
+  } catch (e) {
+    console.log(e.message);
+    return { props: {} };
+  }
+};
 
 export default name;
